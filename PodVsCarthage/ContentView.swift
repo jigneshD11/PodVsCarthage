56b471ede73c79280cdcded3254c534698670321
jignesh <jignesh@dream11.com>
2020-02-02 18:40:25 +0530

    Initial Commit

---
+++ PodVsCarthage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PodVsCarthage
+//
+//  Created by Jignesh on 02/02/20.
+//  Copyright © 2020 Jignesh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
