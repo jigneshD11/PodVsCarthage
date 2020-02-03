6ac9800c1e6fec8fb895f5a26031f4d0663e1227
jignesh <jignesh@dream11.com>
2020-02-03 20:00:14 +0530

    fix(podFolder): Removed Pod folder

--- Pods/Target Support Files/FlowKitManager/FlowKitManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FlowKitManager : NSObject
-@end
-@implementation PodsDummy_FlowKitManager
-@end
--- Pods/Target Support Files/Pods-PodVsCarthage-PodVsCarthageUITests/Pods-PodVsCarthage-PodVsCarthageUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PodVsCarthage_PodVsCarthageUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PodVsCarthage_PodVsCarthageUITests
-@end
